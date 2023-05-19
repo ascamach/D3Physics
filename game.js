058575ee3a4e3e5aefa079b85239d2d6e7ad0533
Addison Camacho <ascamach@ucsc.edu>
2023-05-18 23:33:11 -0700

    Changing game, returning to default

--- game.js
+++ game.js
@@ -4,30 +4,10 @@ class Intro extends Phaser.Scene {
     }
 
     preload() {
-        this.load.tilemapTiledJSON('first_level', 'assets/first_level.json');
-        this.load.image('tiles', 'assets/tiles.png', {
-            frameWidth: 16,
-            frameHeight: 16,
-        });
     }
 
     create() {
         this.add.text(50, 50, 'Hello World').setFontSize(50);
-
-        const map = this.make.tilemap({key: 'first_level'});
-
-        this.tiles = this.map.addTilesetImage('tiles', 'tiles');
-
-        groundLayer = map.createDynamicLayer('World', groundTiles, 0, 0);
-
-        groundLayer.setCollisionByExclusion([-1]);
-
-        this.physics.world.bounds.width = groundLayer.width;
-        this.physics.world.bounds.height = groundLayer.height
-    }
-
-    update() {
-        
     }
 }
 
