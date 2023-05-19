1477dc3de08df13651c08cfa7f48a2257cea2644
Addison Camacho <ascamach@ucsc.edu>
2023-05-19 00:45:51 -0700

    added physics interaction between enemy and laser

--- game.js
+++ game.js
@@ -6,14 +6,17 @@ class Intro extends Phaser.Scene {
     preload() {
         this.load.path = "./assets/";
         this.load.image("ship", "ship.png");
+        this.load.image("enemy", "enemy.png");
     }
 
     create() {
         this.add.text(50, 50, 'Hello World').setFontSize(50);
 
-        let ship = this.physics.add.sprite(400, 500, "ship")
+        let ship = this.physics.add.sprite(400, 700, "ship")
             .setScale(1.25)
             .setCollideWorldBounds(true);
+
+        let enemy = this.physics.add.image(400, 100, "enemy");
 
         this.cursors = this.input.keyboard.createCursorKeys();
 
@@ -35,15 +38,27 @@ class Intro extends Phaser.Scene {
 
         this.input.on('pointerdown', () => {
             let laser = this.physics.add.image(ship.x+4, ship.y, "laser").setVelocityY(-500);
-            // this.time.delayedCall(6000, laser.destroy());
+
+            this.physics.add.collider(laser, enemy, destroy_enemy, null, this);
+
+            if (laser.y > this.game.config.height) {
+                laser.destroy;
+            } 
+
+            
         });
     }
+}
+
+function destroy_enemy(laser, enemy) {
+    laser.destroy();
+    enemy.destroy();
 }
 
 const game = new Phaser.Game({
     type: Phaser.AUTO,
     width: 800,
-    height: 600,
+    height: 800,
 
     physics: {
         default: 'arcade',
