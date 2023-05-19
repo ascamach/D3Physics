847286213b732f2f20ca4482f4a0080f2460d7ed
Addison Camacho <ascamach@ucsc.edu>
2023-05-19 00:27:58 -0700

    Adding basic movement and laser shoot function

--- game.js
+++ game.js
@@ -4,10 +4,39 @@ class Intro extends Phaser.Scene {
     }
 
     preload() {
+        this.load.path = "./assets/";
+        this.load.image("ship", "ship.png");
     }
 
     create() {
         this.add.text(50, 50, 'Hello World').setFontSize(50);
+
+        let ship = this.physics.add.sprite(400, 500, "ship")
+            .setScale(1.25)
+            .setCollideWorldBounds(true);
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        this.input.keyboard.on('keydown', (event) => {
+            if (event.key == 'a') {
+                ship.setVelocityX(-200);
+            } else if (event.key == 'd') {
+                ship.setVelocityX(200);
+            } else {
+                ship.setVelocityX(0);
+            }
+        });
+
+        this.input.keyboard.on('keyup', (event) => {
+            if(event.key) {
+                ship.setVelocityX(0);
+            }
+        });
+
+        this.input.on('pointerdown', () => {
+            let laser = this.physics.add.image(ship.x+4, ship.y, "laser").setVelocityY(-500);
+            // this.time.delayedCall(6000, laser.destroy());
+        });
     }
 }
 
@@ -19,8 +48,7 @@ const game = new Phaser.Game({
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: {y: 500},
-            debug: false
+            degug: false
         }
     },
 
