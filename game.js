99b529c2c8f77b0d3345a2101109789c4a55ff03
Addison Camacho <ascamach@ucsc.edu>
2023-05-18 23:16:37 -0700

    trying some tests with tilemaps

--- game.js
+++ game.js
@@ -3,8 +3,31 @@ class Intro extends Phaser.Scene {
         super('intro');
     }
 
+    preload() {
+        this.load.tilemapTiledJSON('first_level', 'assets/first_level.json');
+        this.load.image('tiles', 'assets/tiles.png', {
+            frameWidth: 16,
+            frameHeight: 16,
+        });
+    }
+
     create() {
         this.add.text(50, 50, 'Hello World').setFontSize(50);
+
+        const map = this.make.tilemap({key: 'first_level'});
+
+        this.tiles = this.map.addTilesetImage('tiles', 'tiles');
+
+        groundLayer = map.createDynamicLayer('World', groundTiles, 0, 0);
+
+        groundLayer.setCollisionByExclusion([-1]);
+
+        this.physics.world.bounds.width = groundLayer.width;
+        this.physics.world.bounds.height = groundLayer.height
+    }
+
+    update() {
+        
     }
 }
 
